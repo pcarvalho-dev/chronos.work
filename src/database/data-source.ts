63c725207d91ab178ecda521a928a65889b239ae
Pablo Carvalho <pablocarvalho.dev@gmail.com>
2025-10-25 11:26:19 -0300

    fix ipv4

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -24,7 +24,9 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USERNAME!,
     password: process.env.DB_PASSWORD!,
     database: process.env.DB_DATABASE!,
-    family: 4,
+    extra: {
+        family: 4
+    },
     synchronize: false,
     logging: false,
     entities: [User, UserCheckIn],
